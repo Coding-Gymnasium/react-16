ab61acc5f4c67ca4498a2650510d54db41c79759
Nico Rithner <nicorithner@gmail.com>
2021-08-12 08:13:53 -0600

    Simply components using new features from React 16

--- src/App.js
+++ src/App.js
@@ -1,28 +1,22 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 import StudentForm from './components/StudentForm';
 import ErrorBoundary from './components/ErrorBoundary';
 import Teachers from './Teachers';
+import Header from './Header';
+import Footer from './Footer';
 
 class App extends Component {
 
   render() {
-    return (
+    return [
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-
-        <ErrorBoundary>
-          <StudentForm />
-        </ErrorBoundary>
-
-        <Teachers />
+        <Header key="head" />  
+        <Teachers key="teach" />
+        <Footer key="foot" />
       </div>
-    );
+    ];
   }
 }
 
